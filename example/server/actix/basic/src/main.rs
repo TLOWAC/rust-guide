d9804faed3ef15584678bda43cb5b5234d925bb3
Changhun Lee <ch.lee@danbicorp.com>
2022-12-09 02:48:08 +0900

    refactor: .env 파일 입력 방식 추가. RUST_ENV 값 참조

--- example/server/actix/basic/src/main.rs
+++ example/server/actix/basic/src/main.rs
@@ -2,12 +2,15 @@ mod config;
 mod controller;
 
 use actix_web::{App, HttpServer};
-use dotenvy::dotenv;
+use dotenvy::from_filename;
 use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-        dotenv().ok();
+        // NOTE: RUST_ENV[local/development/production] 환경변수 설정
+        let env_path = format!(".env.{}", env::var("RUST_ENV").unwrap());
+
+        from_filename(env_path).unwrap();
 
         let config = crate::config::Settings::from_env().unwrap();
 
