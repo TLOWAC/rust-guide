47c350b29acce96132284fc11700b698290aa0a0
Changhun Lee <ch.lee@danbicorp.com>
2022-12-09 02:23:38 +0900

    refactor: env configuration 추가

--- example/server/actix/basic/src/main.rs
+++ example/server/actix/basic/src/main.rs
@@ -1,11 +1,21 @@
+mod config;
 mod controller;
 
 use actix_web::{App, HttpServer};
+use dotenvy::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(controller::getUser))
-        .bind(("127.0.0.1", 9000))?
+        dotenv().ok();
+
+        let config = crate::config::Settings::from_env().unwrap();
+
+        HttpServer::new(|| {
+                let app = App::new().service(controller::get_user);
+                return app;
+        })
+        .bind((config.server.host, config.server.port))?
         .run()
         .await
 }
