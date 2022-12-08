bb8e04246961b537d2a1d2fb27086b2e3799e809
Changhun Lee <ch.lee@danbicorp.com>
2022-12-09 02:25:19 +0900

    chore: 변수명 수정 camel -> snake

--- example/server/actix/basic/src/controller.rs
+++ example/server/actix/basic/src/controller.rs
@@ -5,18 +5,18 @@ use serde::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
-    name: String,
-    age: u32,
-    company: String,
+        name: String,
+        age: u32,
+        company: String,
 }
 
 #[get("/api/{name}")]
-pub async fn getUser(name: Path<String>) -> Result<impl Responder> {
-    let user = User {
-        name: name.to_string(),
-        age: 25,
-        company: String::from("danbicorp"),
-    };
+pub async fn get_user(name: Path<String>) -> Result<impl Responder> {
+        let user = User {
+                name: name.to_string(),
+                age: 25,
+                company: String::from("danbicorp"),
+        };
 
-    Ok(Json(user))
+        Ok(Json(user))
 }
