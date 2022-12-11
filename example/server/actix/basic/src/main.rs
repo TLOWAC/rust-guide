b4ebf0c95039a439f01f1a0fff417321eb9e0fe6
Changhun Lee <ch.lee@danbicorp.com>
2022-12-12 01:20:20 +0900

    feat(app): middleware logger 추가

--- example/server/actix/basic/src/main.rs
+++ example/server/actix/basic/src/main.rs
@@ -1,24 +1,32 @@
 mod config;
 mod controller;
 
-use actix_web::{App, HttpServer};
+use crate::config::Settings;
+
+use actix_web::{middleware::Logger, App, HttpServer};
 use dotenvy::from_filename;
+use env_logger::Env;
 use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
         // NOTE: RUST_ENV[local/development/production] 환경변수 설정
         let env_path = format!(".env.{}", env::var("RUST_ENV").unwrap());
-
         from_filename(env_path).unwrap();
 
-        let config = crate::config::Settings::from_env().unwrap();
+        let config = Settings::from_env().unwrap();
+        let Settings { server, .. } = config;
+
+        env_logger::init_from_env(Env::default().default_filter_or("info"));
 
         HttpServer::new(|| {
-                let app = App::new().service(controller::get_user);
+                let app = App::new()
+                        .wrap(Logger::new("%a %r %s %T"))
+                        .service(controller::get_user);
+
                 return app;
         })
-        .bind((config.server.host, config.server.port))?
+        .bind((server.host, server.port))?
         .run()
         .await
 }
